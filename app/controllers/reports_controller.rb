3f1df24d2e06ccf6ac113945aa1e4215ecec610f
KD <kasia.dymarek@ontc.pl>
2016-12-02 18:21:32 +0100

    refactor code in routes.rb

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,5 @@ class ReportsController < ApplicationController
   expose(:subject_items) { SubjectItem.includes(:teacher, :students) }
   
   def subjects
-    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root :to => "visitors#index"
+  root "visitors#index"
 
   devise_for :user
 
